d055651789766adeb2a2c7ad3cc18ca0774d7c79
Andrei Z <b1naryth1ef@gmail.com>
2011-04-10 12:21:19 -0500

    V0.1.4 -- configobj.py ADDED: The source was taken from here: http://bit.ly/eauaQx diskchecker.py EDITED: Now uses while true: for better processing. Uses configobj too import the settings.cfg file. doemail is broken in this version due too bug #2 doemail2 which is our smtp backend is now preferred by the script. example_settings.cfg ADDED: The file holding all our smtp/email/path settings. Edit this and rename it settings.cfg mailer.py DELETED: README.markdown EDITED: More description... Added refrence too configobj Etc.
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- README.markdown
+++ README.markdown
@@ -2,4 +2,12 @@
 
 ![PyDskChk Command Line](http://i.imgur.com/VNIUF.jpg)
 
-PyDskChk is a simple python disk checking system to email you when disks or folders become unavailable. It was developed by [Andrei Zbikowski](http://az.wbbmx.org/) and was written to help manage large raid disk drives. It's free and open source, and probably 10x worse then any other disk checking system. 
+PyDskChk or Python Disk Checker is a simple Python only system for monitoring disks or folders. It has both local functionality (just alerting a terminal (and soon a file)) and email functionality. It simply takes a path and monitors it, when the path becomes unavalible it will print to the window and email you (through SMTP) with an alert.
+PyDskChk was developed for UZ's large a** RAID system (Over 300 disks to monitor) and is not _currently_ in use, although use is planned in the future. It was written and developed by [Andrei Zbikowski](http://az.wbbmx.org/) and was written to help manage large raid disk drives. It's free and open source, and probably 10x worse then any other disk checking system. 
+==Thanks To==
+PyDskChk deserves credit to a few people...
+Bob2 on Freenode's Python for recommending while True:
+
+PyDskChk uses a few systems...
+[ConfigObj](http://bit.ly/eauaQx) was written by Michael Foord and Nicola Larosa. It's currently used for parsing settings.cfg into our diskchecker.py
+[SMTPLib](http://effbot.org/librarybook/smtplib.htm) a included module in Python 2.5 and later, it's the core to the email system.
--- diskchecker.py
+++ diskchecker.py
@@ -1,34 +1,51 @@
 import smtplib
 import string
 import os
-import mailer
+#import mailer
 import email.Utils
 from email.mime.text import MIMEText
+from configobj import ConfigObj
 
-#---------------EDIT BELOW------------------#
-SENDMAIL = "/usr/sbin/sendmail" # sendmail location
-path = "./test" #Path of disk to monitor
-email = "ateamrocks@gmail.com" #Your email
-sentfrom = "azcomp@passthrough.com"
-#fp = open("emailfile", 'wb+rb')
-
-#--------DO NOT EDIT BELOW THIS LINE--------#
-
-
+                                                                   #If you got here, please go and edit settings.py !!!!
+config = ConfigObj("settings.cfg")                                 #Although this isnt technically a .py file, it's just easy for packaging.
+sendmail = config['sendmail']
+path = config['path']
+email = config['email']
+sentfrom = config['sentfrom']
+smtp = config['smtp']
+username = config['username']
+password = config['password']
+smtpserver = config['server']
 x = "0"
+
+
+
 def printer(prints):
 	if x == "0":
 		print prints
 	else:
 		None
-	
+def doemail2(location,reason):
+	func = "There is a disk or folder error (001) on disk/folder: "+path+" ."
+	msg = MIMEText(func)
+	msg['Subject'] = 'Temperature'
+	msg['From'] = sentfrom
+	msg['To'] = email     # functions to send an email
+	server = smtplib.SMTP(smtpserver)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login(username,password)
+	server.sendmail(sentfrom, email, msg.as_string())
+	server.quit()
+		
 def doemail(location,reason):
-	p = os.popen("%s -t" % SENDMAIL, "w")
-	p.write("To:%s\n" % email)  
-	p.write("Subject: DISK ERROR ON DISK %s\n" % path)
-	p.write("\n") # blank line separating headers from body
-	p.write("PATH: "+path+"\n")
-	p.write("REASON: "+reason+" \n")
+	p = os.popen("%s -t" % sendmail, "w")
+	p.write("To:%s\\n" % email)  
+	p.write("Subject: DISK ERROR ON DISK %s\\n" % path)
+	p.write("\\n") # blank line separating headers from body
+	p.write("PATH: "+path+"\\n")
+	p.write("REASON: "+reason+" \\n")
 	sts = p.close()
 	if sts != 0:
 		print "Sendmail exit status", sts
@@ -41,10 +58,10 @@ while True:
 		printer("Disk/Folder Not Found!")
 		x = "1"
 		reason = "Not Found!"
-		doemail(path,reason)
+		doemail2(path,reason)
 		break
 	else:
-		doemail(path,reason)
+		doemail2(path,reason)
 		print "ERRORZ"
 		reason = "Random error!"
 		
